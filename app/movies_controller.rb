20ae46b7f3b9d788a6cd100ef58e93c14c9d042f
Carol Greene <carolgreene3@gmail.com>
2018-03-20 21:48:38 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,7 @@ def can_be_created_with_a_hash_of_attributes
   movie = _
 end
 
-def can_be_created_in_a_block(args = {_ })
+def can_be_created_in_a_block(args = { })
   Movie.create do |m|
   _
   _
